47244e572b5c00ee144dfb28d7715fcfeb8e5505
Oliver Evans <oge1@zips.uakron.edu>
2016-12-15 20:55:22 -0500

    Update ffmpeg script. Really works now!

--- scripts/run_ffmpeg.sh
+++ scripts/run_ffmpeg.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-ffmpeg -i elec_img/elec_quad_osc_%03d.png -c:v libx264 -crf 24 -pix_fmt yuv420p -r 30 -framerate 30 -t 00:00:08 movies/elec_test.mp4
+imgdir=$1
+outfile=$2
+nloops=$3
+rm -rf tmp
+mkdir tmp -p
+k=0
+echo "Copying files"
+for i in `seq 1 $nloops`
+do
+	# Skip first image
+	for f in `ls $imgdir | awk '$1 !~ /001/{print}'`
+	do
+		name=`printf 'img%04d.png' $k`
+		k=$((k+1))
+		rm -rf tmp/$name
+		cp $imgdir/$f tmp/$name
+	done
+done
+
+echo "Converting to movie"
+ffmpeg -i tmp/img%04d.png -c:v libx264 -crf 24 -pix_fmt yuv420p -r 30 -framerate 30 -y $outfile
+rm -rf tmp
+
+echo "Done!"
